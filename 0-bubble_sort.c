a8045770f53c9c9ea6b548a013b2015977208560
Marwwagalal204 <Marwwagalal204@gmail.com>
2024-01-19 07:31:13 +0200

    task0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function to sort an array in bubble sort
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i, n, flag, temp;
+
+if (array == NULL || size == 0)
+return;
+
+for (i = 0; i < size - 1; i++)
+{
+flag = 0;
+for (n = 0; n < size - 1 - i; n++)
+{
+if (array[n] > array[n + 1])
+{
+temp = array[n];
+array[n] = array[n + 1];
+array[n + 1] = temp;
+flag = 1;
+print_array(array, size);
+}
+}
+if (flag == 0)
+break;
+}
+}
