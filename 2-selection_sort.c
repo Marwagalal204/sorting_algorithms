67b7060f34da8de4a82d7b70679d42685e1c8956
Marwwagalal204 <Marwwagalal204@gmail.com>
2024-01-20 09:07:25 +0200

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting an array using selection sort
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+void selection_sort(int *array, size_t size)
+{
+size_t min, i, j, temp;
+
+if (array == NULL || size == 0)
+return;
+
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+
+for (j = 0; j < size; j++)
+{
+if (array[min] > array[j])
+min = j;
+}
+if (array[min] != array[i])
+{
+temp = array[i];
+array[i] = array[min];
+array[min] = temp;
+print_array(array, size);
+}}}
